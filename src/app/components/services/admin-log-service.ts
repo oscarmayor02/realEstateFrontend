bb1ae6a2bb7e69fd99750eefdf689df2f99f7a34
Oscar Mayor <oscar.mayor.dev@gmail.com>
2025-08-05 01:15:43 +1200

    Build listo para producción

--- src/app/components/services/admin-log-service.ts
+++ src/app/components/services/admin-log-service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AdminLog } from '../../models/AdminLog.model';
 import { Observable } from 'rxjs';
+import { environment } from '../../../enviroments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminLogService {
-  private apiUrl = 'http://localhost:8080/api/admin/logs';
-
+  // private apiUrl = 'http://localhost:8080/api/admin/logs';
+  private apiUrl = `${environment.apiBaseUrl}/admin/logs`;
   constructor(private http: HttpClient) {}
 
   getLogs(): Observable<AdminLog[]> {
--- src/app/components/services/favorite-service.ts
+++ src/app/components/services/favorite-service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../enviroments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FavoriteService {
-  private apiUrl = 'http://localhost:8080/api/favorites';
-
+  // private apiUrl = 'http://localhost:8080/api/favorites';
+  private apiUrl = `${environment.apiBaseUrl}/favorites`;
   constructor(private http: HttpClient) {}
 
   getFavoritesByUser(userId: number): Observable<any[]> {
